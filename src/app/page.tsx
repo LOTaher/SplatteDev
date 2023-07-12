27ac321996fb60382ff455cdbaf5e6c35423fbe3
Laith Taher <86690869+LOTaher@users.noreply.github.com>
2023-07-12 01:19:22 -0700

    homepage looking better

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,19 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import GithubButton from "@/components/GithubButton";
 import LinkButton from "@/components/LinkButton";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main className="flex justify-center h-screen">
       <div className="h-full w-full md:max-w-2xl">
         <div className="content-start flex-col">
-          <h1 className="mb-8 mt-8">Hey, I&apos;m Laith</h1>
+          <h1 className="mb-8">Hey, I&apos;m Laith</h1>
           <div className="flex content-center space-x-3">
             <Image
               src="https://media.licdn.com/dms/image/D4E03AQECes9egjScKw/profile-displayphoto-shrink_800_800/0/1668552656586?e=1694649600&v=beta&t=YChvWMfOS6TA28rEscX06DIeC6oJdhYDpa4reBs07P0"
@@ -51,7 +51,7 @@ export default function Home() {
             <Card className="rounded mb-8">
               <CardHeader>
                 <CardTitle className="text-white">FreeHand</CardTitle>
-                <CardDescription className="text-[#8e8f93">
+                <CardDescription className="text-[#8e8f93]">
                   Open Source Illustrations designed to elevate your next
                   project.
                 </CardDescription>
@@ -66,7 +66,7 @@ export default function Home() {
             <Card className="rounded mb-8">
               <CardHeader>
                 <CardTitle className="text-white">splatte.dev</CardTitle>
-                <CardDescription className="text-[#8e8f93">
+                <CardDescription className="text-[#8e8f93]">
                   My personal website built using Next.js, Tailwind CSS, and
                   Vercel.
                 </CardDescription>
@@ -80,7 +80,7 @@ export default function Home() {
             <Card className="rounded mb-8">
               <CardHeader>
                 <CardTitle className="text-white">BeGroovy</CardTitle>
-                <CardDescription className="text-[#8e8f93">
+                <CardDescription className="text-[#8e8f93]">
                   A music sharing social media website to upgrade your music
                   collection.
                 </CardDescription>
@@ -92,7 +92,7 @@ export default function Home() {
               </CardContent>
             </Card>
           </div>
-          <h1 className="mt-8 mb-8">Blog</h1>
+          <Footer />
         </div>
       </div>
     </main>
